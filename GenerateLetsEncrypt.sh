31ea8b3559bb9772924b16ac7c26681645971daf
zikum <info@zikum.cz>
2019-02-27 21:41:05 +0100

    Typo corrections

--- GenerateLetsEncrypt.sh
+++ GenerateLetsEncrypt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#Generates Let's Encrypt requests for selected domains using Webmin LLet's Encrypt subsystem 
+#Generates Let's Encrypt requests for selected domains using Webmin Let's Encrypt subsystem 
 #Addressing issue in Webmin/Virtualmin with Let's Encrypt auto renew function (just workaround)
-#Intended to be run via crontab within reasonable period (once per two months)
-#v0.1 - initial quick version
+#Intended to be run via crontab within reasonable time period (once per two months)
+#v0.1 - initial quick version without logging
 
 #Get fresh list of domains with SSL enabled (master domains, subservers and aliases)
 Domains=`virtualmin list-domains --with-feature ssl |awk '{print $1}' |tail -n+3`
@@ -11,7 +11,6 @@ Domains=`virtualmin list-domains --with-feature ssl |awk '{print $1}' |tail -n+3
 echo "$Domains" > Domains.list
 
 #Request certificate for each domain
-
 while IFS= read -r domain
 do
 virtualmin generate-letsencrypt-cert --domain "$domain"
