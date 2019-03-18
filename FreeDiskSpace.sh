379f4d59b4f59b7425761ed66188925fd4ff6f91
zikum <info@zikum.cz>
2019-03-18 17:22:15 +0100

    Typo corrections

--- FreeDiskSpace.sh
+++ FreeDiskSpace.sh
@@ -3,8 +3,8 @@
 # When free space drops below set threshold it deletes all defined files in given paths
 # v1.0 - Final basic version
 
-# Set treshold in %
-TRESHOLD=40
+# Set threshold in %
+THRESHOLD=40
 # Dir with log files
 DIRLOG=/application/
 #Files to delete
@@ -12,21 +12,21 @@ FILES=*.gz
 
 
 # Watch all logging FS for threshold. If its okay, then just do nothing and exit... Else, parse them and CLEAN them!
-FILESYSTEMS=$(df -P 2> /dev/null |grep log |awk '0+$5 >= '$TRESHOLD' {print}' |awk '{ print $6 }')
+FILESYSTEMS=$(df -P 2> /dev/null |grep log |awk '0+$5 >= '$THRESHOLD' {print}' |awk '{ print $6 }')
 
 if [ "$FILESYSTEMS" = "" ]; then
-    echo "No FS met treshold - All okay exiting...";
+    echo "No FS met threshold - All okay exiting...";
 
 exit 1;
 
 else
 
 # Save results, add app log dir and file extension
-FSOVERTRESHOLD=$FILESYSTEMS
+FSOVERTHRESHOLD=$FILESYSTEMS
 
 FILESTODELETE=$(while read -r line; do
 	    		echo "$line$DIRLOG"
-		done <<< "$FSOVERTRESHOLD")
+		done <<< "$FSOVERTHRESHOLD")
 
 
 # CLEAN files!
